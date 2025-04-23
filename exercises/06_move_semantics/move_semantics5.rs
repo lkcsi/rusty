f3de7e8d50419d30d25273cb3061930c63903e07
roland <lukacsiroland01@gmail.com>
2025-04-23 22:43:15 +0200

    initial commit

---
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -0,0 +1,26 @@
+#![allow(clippy::ptr_arg)]
+
+// TODO: Fix the compiler errors without changing anything except adding or
+// removing references (the character `&`).
+
+// Shouldn't take ownership
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
+
+    println!("{data}");
+}
+
+fn main() {
+    let data = "Rust is great!".to_string();
+
+    let a = get_char(&data);
+    println!("a: {}", a);
+    println!("data: {}", data);
+
+    string_uppercase(data);
+}
---
+++ exercises/18_iterators/iterators4.rs
@@ -0,0 +1,35 @@
+fn factorial(num: u64) -> u64 {
+    if num == 0 {
+        1
+    } else {
+        factorial(num-1) * num
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(factorial(1), 1);
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(factorial(4), 24);
+    }
+}
